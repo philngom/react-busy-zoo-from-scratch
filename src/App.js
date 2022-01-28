6d0184faa1336f0a4bc3b4db44dc39a011054019
Philippe Ngom <philngom@gmail.com>
2022-01-28 11:16:38 -0800

    added Shop folder, passed down props from App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { useState } from 'react';
 import AnimalFight from './AnimalFight/AnimalFight';
+import Shop from './Shop/Shop';
 
 function App() {
   const [unicornSize, setUnicornSize] = useState(10);
   const [lionSize, setLionSize] = useState(10);
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
     <div className="App">
       <AnimalFight unicornSize={unicornSize} setUnicornSize={setUnicornSize} lionSize={lionSize} setLionSize={setLionSize}/>
-      <div>second row</div>
+      <Shop isOpen={isOpen} setIsOpen={setIsOpen}/>
       <div>third row</div>
     </div>
   );
---
+++ src/Shop/Shop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Shop.css';
+
+export default function Shop() {
+  return (
+    <div>
+
+    </div>
+  );
+}
