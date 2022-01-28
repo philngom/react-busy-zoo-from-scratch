1920fbc6dc83b7668584cf208ea4383ac3f22140
Philippe Ngom <philngom@gmail.com>
2022-01-28 11:25:50 -0800

    shop open/close functionality working

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Shop.css';
 
-export default function Shop() {
+export default function Shop({ isOpen, setIsOpen }) {
   return (
-    <div>
-
+    <div className='shop'>
+      <div className='door'>
+        The shop is {isOpen ? 'open!' : 'closed!'}
+      </div>
+      <div>
+        <button onClick={() => setIsOpen(true)}>Open Shop</button>
+        <button onClick={() => setIsOpen(false)}>Close Shop</button>
+      </div>
     </div>
   );
 }
