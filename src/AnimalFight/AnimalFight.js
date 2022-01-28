7f827967120941bebf9f24435978b19c530558a5
Philippe Ngom <philngom@gmail.com>
2022-01-28 11:00:53 -0800

    AnimalFight functionality complete

--- src/AnimalFight/AnimalFight.js
+++ src/AnimalFight/AnimalFight.js
@@ -10,15 +10,15 @@ export default function AnimalFight({ setLionSize, setUnicornSize, lionSize, uni
       <div className="lion">
         <img src={'./images/lion.jpeg'} style={{ width: lionSize * 10 }}></img>
         <div className="buttons">
-          <button className='center'>The lion is growing!</button>
-          <button>The lion is munching down on the unicorn!</button>
+          <button className='center' onClick={() => setLionSize(lionSize + 1)}>The lion is growing!</button>
+          <button onClick={() => setUnicornSize(unicornSize - 1)}>The lion is munching down on the unicorn!</button>
         </div>
       </div>
       <div className="unicorn">
         <img src={'./images/unicorn.jpeg'} style={{ width: unicornSize * 10 }}></img>
         <div>
-          <button className='center'>The unicorn is growing!</button>
-          <button>The unicorn is munching down on the lion!</button>
+          <button className='center' onClick={() => setUnicornSize(unicornSize + 1)}>The unicorn is growing!</button>
+          <button onClick={() => setLionSize(lionSize - 1)}>The unicorn is munching down on the lion!</button>
         </div>
 
       </div>
